e160121ed1985cc390986f4f63ee18602ad8aaa7
baeluna <lubnamarliana3@gmail.com>
2026-06-18 20:27:38 +0700

    Initial commit

---
+++ src/main/java/com/pbo/arungi/ArungiApplication.java
@@ -0,0 +1,18 @@
+package com.pbo.arungi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArungiApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(
+                ArungiApplication.class,
+                args
+        );
+
+    }
+
+}
---
+++ src/main/java/com/pbo/arungi/controller/HomeController.java
@@ -0,0 +1,49 @@
+package com.pbo.arungi.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+
+    @GetMapping("/")
+    public String home(){
+
+        return "index";
+
+    }
+
+
+    @GetMapping("/destinations")
+    public String destinations(){
+
+        return "destinations";
+
+    }
+
+
+    @GetMapping("/packages")
+    public String packages(){
+
+        return "packages";
+
+    }
+
+
+    @GetMapping("/login")
+    public String login(){
+
+        return "login";
+
+    }
+
+
+    @GetMapping("/register")
+    public String register(){
+
+        return "register";
+
+    }
+
+}
